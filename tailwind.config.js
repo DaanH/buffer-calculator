66918c7cc294d9c32c54c002f75e644cc0c445ca
DaanH <daanheuvingh@gmail.com>
2024-09-13 11:59:30 +0200

    project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./src/**/*.{ts,tsx}"],
-    theme: {
-        extend: {}
-    },
-    plugins: []
+	content: ["./index.html", "./src/**/*.{ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				purple: "#42145f",
+				violet: "#a90061",
+				red: "#d52b1e",
+				rubyRed: "#ca005d",
+				pink: "#f092cd",
+				orange: "#e17000",
+				yellow: "#f9e11e",
+				darkYellow: "#ffb612",
+				brown: "#94710a",
+				darkBrown: "#673327",
+				green: "#39870c",
+				darkGreen: "#275937",
+				mintGreen: "#76d2b6",
+				mossGreen: "#777b00",
+				darkBlue: "#01689b",
+				skyBlue: "#007bc7",
+				lightBlue: "#8fcae7"
+			}
+		}
+	},
+	plugins: []
 };
