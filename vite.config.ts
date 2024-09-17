b520214b09fa2bc6b6736a156f43d85ad157ee05
DaanH <daanheuvingh@gmail.com>
2024-09-17 16:33:26 +0200

    updated lockfile

--- vite.config.ts
+++ vite.config.ts
@@ -2,42 +2,40 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { createHtmlPlugin } from "vite-plugin-html";
 import svgrPlugin from "vite-plugin-svgr";
-import md from "vite-plugin-react-md";
-
-// https://vitejs.dev/config/
+import markdownPlugin from "vite-plugin-react-md";
 
 export default defineConfig(({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-    return {
-        plugins: [
-            react(),
-            createHtmlPlugin({
-                inject: {
-                    data: { MODE: mode }
-                }
-            }),
-            svgrPlugin({
-                svgrOptions: {
-                    icon: true
-                }
-            }),
-            md()
-        ],
-        build: {
-            target: "esnext",
-            manifest: true,
-            rollupOptions: {
-                output: {
-                    manualChunks: undefined
-                }
-            }
-        },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    includePaths: ["src/styles", "src/pages", "src/components"]
-                }
-            }
-        }
-    };
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+	return {
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				inject: {
+					data: { MODE: mode }
+				}
+			}),
+			svgrPlugin({
+				svgrOptions: {
+					icon: true
+				}
+			}),
+			markdownPlugin()
+		],
+		build: {
+			target: "esnext",
+			manifest: true,
+			rollupOptions: {
+				output: {
+					manualChunks: undefined
+				}
+			}
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					includePaths: ["src/styles", "src/pages", "src/components"]
+				}
+			}
+		}
+	};
 });
