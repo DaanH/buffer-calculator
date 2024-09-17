631513722dc4347d0a82cbc5a6e3bf141acb9d0c
DaanH <daanheuvingh@gmail.com>
2024-09-17 17:19:28 +0200

    added step icons to progress bar

--- src/buffer-calculator/Progress.tsx
+++ src/buffer-calculator/Progress.tsx
@@ -1,5 +1,27 @@
+import { ReactNode } from "react";
+import { ReactComponent as AssetsIcon } from "../assets/icons/assets.svg";
+import { ReactComponent as BuildingsIcon } from "../assets/icons/buildings.svg";
+import { ReactComponent as PartnershipIcon } from "../assets/icons/partnership.svg";
+import { ReactComponent as SchoolIcon } from "../assets/icons/school.svg";
+import { ReactComponent as WalletIcon } from "../assets/icons/wallet.svg";
+import { ReactComponent as WarningIcon } from "../assets/icons/warning.svg";
 import { useCalculatorContext } from "./CalculatorContext";
-import { flowSteps } from "./steps";
+import { FlowLabels, flowSteps, Steps } from "./steps";
+
+const iconsByStep: Record<Steps, ReactNode> = {
+	[Steps.Organization]: <SchoolIcon />,
+	[Steps.Capital]: <WalletIcon />,
+	[Steps.Buildings]: <BuildingsIcon />,
+	[Steps.Assets]: <AssetsIcon />,
+	[Steps.Riskbuffer]: <WarningIcon />,
+	[Steps.Result]: <WalletIcon />
+};
+
+const TabIcon = ({ step }: { step: Steps }) => {
+	const { flow } = useCalculatorContext();
+	if (step === Steps.Organization) return flow === FlowLabels.Partnership ? <PartnershipIcon /> : <SchoolIcon />;
+	return iconsByStep[step];
+};
 
 const Progress = () => {
 	const { flow, step } = useCalculatorContext();
@@ -9,13 +31,16 @@ const Progress = () => {
 			{flowSteps[flow].map((flowStep, index) => (
 				<div
 					key={flowStep}
-					className={`w-1 flex items-center justify-center  flex-grow border-mossGreen ${
+					className={`w-1 flex items-center justify-center flex-col gap-2 flex-grow border-mossGreen ${
 						index < currentIndex ? "bg-[#f9f9f9]" : ""
 					} 
 					${index > currentIndex ? "bg-[#e6e6e6]" : ""}
 					${currentIndex === index ? "bg-white border-t-8 mt-0" : "mt-4"}
 					`}
 				>
+					<div className="flex text-lg fill-mossGreen items-center justify-center w-12 h-12 bg-white rounded-full border-2 border-mossGreen">
+						<TabIcon step={flowStep} />
+					</div>
 					{flowStep}
 				</div>
 			))}
