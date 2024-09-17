c6a60ae94c642b86b9d5cfeb155c921e52ccd5ff
DaanH <daanheuvingh@gmail.com>
2024-09-17 16:51:24 +0200

    changed default page to calculator

--- src/hooks/useNav.tsx
+++ src/hooks/useNav.tsx
@@ -1,13 +1,13 @@
 import { createContext, ReactNode, useState, useContext, useMemo } from "react";
 
 interface INav {
-    activePage: string;
-    setActivePage: (page: string) => void;
+	activePage: string;
+	setActivePage: (page: string) => void;
 }
 
 const navContextDefaultState: INav = {
-    activePage: "home",
-    setActivePage: () => null
+	activePage: "calculator",
+	setActivePage: () => null
 };
 
 const NavContext = createContext<INav>(navContextDefaultState);
@@ -17,10 +17,10 @@ export const useNav = () => useContext(NavContext);
 type NavContextProviderProps = { children: ReactNode };
 
 const NavContextProvider = ({ children }: NavContextProviderProps) => {
-    const [activePage, setActivePage] = useState(navContextDefaultState.activePage);
+	const [activePage, setActivePage] = useState(navContextDefaultState.activePage);
 
-    const value = useMemo(() => ({ activePage, setActivePage }), [activePage]);
-    return <NavContext.Provider value={value}>{children}</NavContext.Provider>;
+	const value = useMemo(() => ({ activePage, setActivePage }), [activePage]);
+	return <NavContext.Provider value={value}>{children}</NavContext.Provider>;
 };
 
 export default NavContextProvider;
