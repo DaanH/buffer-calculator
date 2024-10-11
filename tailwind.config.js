4f57a7376ed0e66a49daab047b3671efd2bd8891
DaanH <daanheuvingh@gmail.com>
2024-10-11 10:01:05 +0200

    added tailwind prettier pluging for autosorting classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./index.html", "./src/**/*.{ts,tsx}"],
+	content: ['./index.html', './src/**/*.{ts,tsx}'],
 	theme: {
 		extend: {
 			colors: {
-				purple: "#42145f",
-				violet: "#a90061",
-				red: "#d52b1e",
-				rubyRed: "#ca005d",
-				pink: "#f092cd",
-				orange: "#e17000",
-				yellow: "#f9e11e",
-				darkYellow: "#ffb612",
-				brown: "#94710a",
-				darkBrown: "#673327",
-				green: "#39870c",
-				darkGreen: "#275937",
-				mintGreen: "#76d2b6",
-				mossGreen: "#777b00",
-				darkBlue: "#01689b",
-				skyBlue: "#007bc7",
-				lightBlue: "#8fcae7",
-				background: "#f3f3f3",
+				purple: '#42145f',
+				violet: '#a90061',
+				red: '#d52b1e',
+				rubyRed: '#ca005d',
+				pink: '#f092cd',
+				orange: '#e17000',
+				yellow: '#f9e11e',
+				darkYellow: '#ffb612',
+				brown: '#94710a',
+				darkBrown: '#673327',
+				green: '#39870c',
+				darkGreen: '#275937',
+				mintGreen: '#76d2b6',
+				mossGreen: '#777b00',
+				darkBlue: '#01689b',
+				skyBlue: '#007bc7',
+				lightBlue: '#8fcae7',
+				background: '#f3f3f3',
 
-				main: "#ffb612",
-				button: "#673327"
+				main: '#e17000',
+				button: '#01689b'
 			}
 		}
 	},
