fda6ed31800d16501a6e8579b345ae587381ac66
DaanH <daanheuvingh@gmail.com>
2024-10-11 12:25:27 +0200

    fixed nextStep logic

--- src/buffer-calculator/CalculatorContext.tsx
+++ src/buffer-calculator/CalculatorContext.tsx
@@ -1,6 +1,6 @@
 //react context for keeping track of the current step and the amounts entered by the user in each step
 
-import { createContext, ReactNode, useContext, useMemo, useState } from 'react';
+import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from 'react';
 import { FlowLabels, flowSteps, Steps } from './steps';
 
 export type CalculatorContextType = {
@@ -21,32 +21,34 @@ const CalculatorContextProvider = ({ children }: { children: ReactNode }) => {
 	const [flow, setFlow] = useState(FlowLabels.School);
 	const [vars, setVars] = useState<CalculatorContextType['vars']>({});
 	const [touchedSteps, setTouchedSteps] = useState<CalculatorContextType['touchedSteps']>({});
+	const setStep = useCallback(
+		(index: number) => {
+			setTouchedSteps((old) => ({
+				...old,
+				[flowSteps[flow][currentStep]]: true
+			}));
+			setCurrentStep(index);
+		},
+		[currentStep, flow]
+	);
 
 	const value = useMemo<CalculatorContextType>(
 		() => ({
 			step: flowSteps[flow][currentStep],
 			touchedSteps,
-			nextStep: () => {
-				setTouchedSteps((old) => ({
-					...old,
-					[flowSteps[flow][currentStep]]: true
-				}));
-				setCurrentStep((prev) => prev + 1);
-			},
+			nextStep: () => setStep(currentStep + 1),
 			flow,
 			setFlow: (flow: FlowLabels) => {
-				console.log('setting flow', flow);
 				setFlow(flow);
 				setVars({});
 				setTouchedSteps({});
 			},
-			setStep: (index: number) => setCurrentStep(index),
+			setStep,
 			setVar: (key: string, value: number) => setVars((prev) => ({ ...prev, [key]: value })),
 			vars
 		}),
 		[currentStep, flow, setFlow, vars, touchedSteps]
 	);
-	console.log('rendering context', touchedSteps);
 	return <calculatorContext.Provider value={value}>{children}</calculatorContext.Provider>;
 };
 
